e00a1446e9aca452225575f5633048e7742fe9a8
tristianthedeveloper <71098162+tristianthedeveloper@users.noreply.github.com>
2021-11-07 02:33:52 -0500

    Spelling error correction.

--- Maths/VampireNumber.java
+++ Maths/VampireNumber.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * n number theory, a vampire number (or true vampire number) is a composite natural number with an
+ * In number theory, a vampire number (or true vampire number) is a composite natural number with an
  * even number of digits, that can be factored into two natural numbers each with half as many
  * digits as the original number and not both with trailing zeroes, where the two factors contain
  * precisely all the digits of the original number, in any order, counting multiplicity. The first
